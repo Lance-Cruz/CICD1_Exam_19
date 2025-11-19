d5966773c4b2157bb1f9c060ca765b786d8b0319
LANCECRUZ-STUDENT <g00428876@atu.ie>
2025-11-19 14:18:30 +0000

    Added registration service class with crud functionality

---
+++ src/main/java/com/example/cicd1_exam_19/RegistrationService.java
@@ -0,0 +1,49 @@
+package com.example.cicd1_exam_19;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RegistrationService {
+    private List<Registration> store =new ArrayList<>();
+
+    public Registration create(Registration r) {
+        store.add(r);
+        return r;
+    }
+
+    public Optional<Registration> read(String attendeeName) {
+        for (Registration r : store) {
+            if (r.getAttendeeName().equals(attendeeName)) {
+                return Optional.of(r);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Registration> update (Registration registration, String attendeeName) {
+        for (Registration r : store) {
+            if (r.getAttendeeName().equals(attendeeName)) {
+                r.setAttendeeName(registration.getAttendeeName());
+                r.setEmail(registration.getEmail());
+                r.setTicketCode(registration.getTicketCode());
+                r.setQuantity(registration.getQuantity());
+                return Optional.of(r);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Registration delete(String attendeeName) {
+        for (Registration r : store) {
+            if(r.getAttendeeName().equals(attendeeName)) {
+                store.remove(r);
+                return r;
+            }
+        }
+        return null;
+    }
+}
